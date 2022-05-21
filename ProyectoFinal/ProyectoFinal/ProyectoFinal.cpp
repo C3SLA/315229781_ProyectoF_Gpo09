b0de35323e3a6cd7f6b6c0a9aa8f3e51dcfc64bd
C3SLA <88303024+C3SLA@users.noreply.github.com>
2022-05-20 19:37:15 -0500

    Avances( carga de modelos)

--- ProyectoFinal/ProyectoFinal/ProyectoFinal.cpp
+++ ProyectoFinal/ProyectoFinal/ProyectoFinal.cpp
@@ -706,7 +706,7 @@ int main()
 		cabecera.Draw(lightingShader);
 		//Mueble pequeño2
 		model = glm::mat4(1);
-		model = glm::translate(model, glm::vec3(13.3f, 0.92f, -0.1f));
+		model = glm::translate(model, glm::vec3(13.4f, 0.92f, -0.1f));
 		model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(1.2f, 1.2f, 1.2f));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
